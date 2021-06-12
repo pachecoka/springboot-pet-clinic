1e2440eacad1056d5ab1cfdbe596f353c6bfb8e7
Karolina Pacheco <karolina_cris@hotmail.com>
2021-06-12 20:06:49 -0300

    add interface for repository

--- src/main/java/com/dev/karol/petclinic/core/ports/output/IConsultRepository.java
+++ src/main/java/com/dev/karol/petclinic/core/ports/output/IConsultRepository.java
@@ -1,2 +1,12 @@
-package com.dev.karol.petclinic.core.ports.output;public interface IConsultRepository {
+package com.dev.karol.petclinic.core.ports.output;
+
+import com.dev.karol.petclinic.core.domain.Consult;
+
+import java.util.List;
+
+public interface IConsultRepository {
+    public void addConsult(Consult consult);
+    public void updateConsult(Consult consult);
+    public Consult getConsult(Integer id);
+    public List<Consult> getConsults();
 }
